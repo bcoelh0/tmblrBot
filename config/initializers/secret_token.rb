447b32c810d69a97de87521d5f2d6f20a1c8bf7e
Bruno Coelho <brunocsncoelho@gmail.com>
2013-05-23 22:03:41 +0100

    initial commit + gemfile setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TmblrBot::Application.config.secret_key_base = 'bb3b2e72533b5bb8f3d787098d39eb8bfc747097b1caac55d116ebf12960b9db06b4fe5789a7b8e2591a04580c45db0c34ecabd93d5b3087ee95f9410077fa63'
